ce8dbc0b79dbd78947cb861a82dab0f038a200e2
finlayrossdavie <39996934+finlayrossdavie@users.noreply.github.com>
2024-12-27 14:27:28 +0000

    updates

---
+++ src/main/java/uk/ac/ed/inf/ilp_cw1/Data/OrderStatus.java
@@ -0,0 +1,22 @@
+package uk.ac.ed.inf.ilp_cw1.Data;
+
+/**
+ * the status an order can have
+ */
+public enum OrderStatus {
+
+  /**
+   * it is invalid
+   */
+  INVALID,
+
+  /**
+   * the state is valid
+   */
+  VALID,
+
+  /**
+   * the state is currently undefined
+   */
+  UNDEFINED
+}
---
+++ src/main/java/uk/ac/ed/inf/ilp_cw1/Data/Pizza.java
@@ -0,0 +1,10 @@
+package uk.ac.ed.inf.ilp_cw1.Data;
+
+/**
+ * defines a pizza which can be ordered from a restaurant
+ * @param name is the unique (systemwide) name of a pizza
+ * @param priceInPence is the price of one pizza in pence
+ */
+public record Pizza (String name, int priceInPence){
+
+}
